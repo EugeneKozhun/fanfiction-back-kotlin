f4d905fa3699973d1a05affd3ebc310cab18127a
kozhun <kozhun@rit.va>
2020-11-23 16:50:41 +0300

    Update kotlin version to 1.4.20

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,10 +3,10 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     id("org.springframework.boot") version "2.3.5.RELEASE"
     id("io.spring.dependency-management") version "1.0.10.RELEASE"
-    kotlin("jvm") version "1.4.10"
-    kotlin("plugin.spring") version "1.4.10"
-    kotlin("plugin.jpa") version "1.4.10"
-    kotlin("kapt") version "1.4.10"
+    kotlin("jvm") version "1.4.20"
+    kotlin("plugin.spring") version "1.4.20"
+    kotlin("plugin.jpa") version "1.4.20"
+    kotlin("kapt") version "1.4.20"
 }
 
 group = "com.itransition"
